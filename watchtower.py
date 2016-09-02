2052053005ce67543b75e04929ba2ddcbc1cfa7d
Nate Brune <newton@Andromeda.MANROOM>
2016-09-02 09:45:56 -0400

    Better printing

--- watchtower.py
+++ watchtower.py
@@ -1,11 +1,13 @@
 from piston.steem import Steem
-steem = Steem(wif="WIF")
-ignored = ["comment", "vote", "custom_json", "pow2"]
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+steem = Steem(wif="Posting_WIF_Here")
+ignored = ["comment", "vote", "custom_json", "pow2", "account_create"]
 for c in steem.rpc.block_stream():
     try:
         tx = tx = c['transactions'][0]['operations'][0]  
         if(tx[0] not in ignored):
-            print(tx)
+            pp.pprint(tx)
     except:
         pass
 
